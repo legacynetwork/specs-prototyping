96c2d5a4763bdf3c5795784ff49ad5449de3fe82
v-almonacid <ealmonacid@gmail.com>
2018-09-14 11:01:58 +0200

    feature/ front-end emulator working, inluding 2 encryption layers

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from legacy.contracts import LegacyUserContract
 from secretsharing import SecretSharer, PlaintextToHexSecretSharer
 from cryptography.fernet import Fernet
-from collections import namedtuple
-from hashlib import sha224
+from Crypto.Cipher import AES
+from hashlib import sha256
 from random import choice
 from string import ascii_uppercase, digits
+import base64
 
 # TO-DO: 
 # - n must be at least 2.
-# - remove secret_piece from Beneficiary struct
-# - consider using dict instead of tuple. eg benefs.append({'wallet': wallet, 'url': url...})
-# - do not store secret pieces in contract !
-
-Beneficiary = namedtuple('Beneficiary', ['wallet_address', 'message_url', 'funds_share', 'secret_piece'])
+# - test decrypt
+# - test everything actually
+# - refactoring: put class AESCipher out
+# - deprecate fernet and use AES instead
 
 def store_file_in_ipfs(message):
     # just return a random hash for now
-    return '/ipfs/Qm' + sha224(message).hexdigest()
+    return '/ipfs/Qm' + sha256(message).hexdigest()
 
 # DEPRECATED
 # def get_personal_key(LEN):
@@ -31,12 +31,32 @@ def get_user_address():
     # in practice, provided through metamask
     # returns a pseudo-random string of length LEN using only ASCII uppercase letters and digits
     LEN = 40
-    return '0x' + ''.join(choice(ascii_uppercase + digits) for _ in range(LEN))    
+    return '0x' + ''.join(choice(ascii_uppercase + digits) for _ in range(LEN)) 
+
+class AESCipher:
+
+    def __init__(self, key):        
+        self.key = sha256(key.encode()).digest()
+        self.BLOCK_SIZE = 16 # AES operates on block multiples of 16 bytes (128 bits)
+
+    def encrypt(self, raw):
+        raw = self.pad(raw)        
+        encryption_suite = AES.new(self.key, AES.MODE_ECB)
+        return base64.b64encode(encryption_suite.encrypt(raw)) 
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv )
+        return unpad(cipher.decrypt( enc[16:] ))
+
+    def pad(self, s):
+        return s + (self.BLOCK_SIZE - len(s) % self.BLOCK_SIZE) * chr(self.BLOCK_SIZE - len(s) % self.BLOCK_SIZE)
+
     
-
-
 print "Setting up your Legacy smart contract. Please provide the following information:"
-print "(note: Legacy won't store any kind of sensible data)"
+print "(note: Legacy won't store any kind of sensible data)\n"
+print "Step 1: Your Beneficiaries"
 
 beneficiaries_tmp = []
 personal_keys = []
@@ -44,45 +64,46 @@ finished = False
 finished_input = ""
 i = 0
 while not finished:    
-    address_i = raw_input("Ethereum wallet address of beneficiary " + str(i) + ":" )
-    message_i = raw_input("What secret message would you like to leave to this person:")
+    address_i = raw_input("Ethereum wallet address of beneficiary " + str(i) + ":\n" )
+    message_i = raw_input("What secret message would you like to leave to this person:\n")
     personal_key_i = get_personal_key()
     personal_keys.append(personal_key_i)
     cipher_suite = Fernet(personal_key_i)
-    message_url_i = store_file_in_ipfs(cipher_suite.encrypt(message_i))
-    funds_share_i = raw_input("Share of your funds that you'd like to transfer to this beneficiary:" )
-    beneficiaries_tmp.append(Beneficiary(address_i, message_url_i, funds_share_i, ''))
-    finished_input = raw_input("Have you finished filling this form (yes/no)?")
+    enc_message_i = cipher_suite.encrypt(message_i)
+    funds_share_i = raw_input("Share of your funds that you'd like to transfer to this beneficiary:\n" )
+    #beneficiaries_tmp.append(Beneficiary(address_i, message_url_i, funds_share_i, ''))
+    beneficiaries_tmp.append({'wallet_address': address_i, 'enc_message': enc_message_i, 'funds_share': funds_share_i})
+    finished_input = raw_input("Any more beneficiaries to include (yes/no)?\n")
     while finished_input not in ['yes', 'no']:
-        finished_input = raw_input("Not a valid answer. Try again")
+        finished_input = raw_input("Not a valid answer. Try again\n")
     finished = True if finished_input == 'yes' else False    
     i = i + 1
 
+print "\nStep 2: The Shared Key"
 n = i # number of beneficiaries
-secret = raw_input("Now, enter a random phrase that will be used to create your secret password (eg. 'I love cats').") 
+secret = raw_input("Now, enter a random phrase that will be used to create your secret password (eg. 'I love cats').\n") 
 secret = secret.lower()
-print "Your secret will be shared among " + str(n) + " persons. How many of them will be required to restore it?"
-k = raw_input("Please enter an integer k such that 2 <= k < " + str(n))
-print "(k,n)=" + "(" + str(k) + "," + str(n) + ")" # DEBUG
-print secret
+print "Your secret will be shared among " + str(n) + " persons. How many of them will be required to restore it?\n"
+k = raw_input("Please enter an integer k such that 2 <= k < " + str(n) + "\n")
 secret_pieces = PlaintextToHexSecretSharer.split_secret(secret, int(k), n)
+# create encryption object based on shared secret
+aes_cipher = AESCipher(secret)
 
-# TODO: remove this block. secrete pieces are not stored by the system !
-# save secrete pieces in each beneficiary struct
 beneficiaries = []
-for i in range (0, n):
-    # fetch data stored in temporal struct 
-    wallet_address_i = beneficiaries_tmp[i].wallet_address
-    message_url_i = beneficiaries_tmp[i].message_url
-    funds_share_i = beneficiaries_tmp[i].funds_share
-    # get secrete share corresponding to i beneficiary
-    secret_piece_i = secret_pieces[i]
-    # save the beneficiary struct in final array
-    beneficiaries.append(Beneficiary(wallet_address_i, message_url_i, funds_share_i, secret_piece_i))
+for i in range (0, n):    
+    # now that we have the shared secret, apply an additional encryption layer
+    doub_enc_message_i = aes_cipher.encrypt(beneficiaries_tmp[i]['enc_message'])    
+    message_url_i = store_file_in_ipfs(doub_enc_message_i)
+    address_i = beneficiaries_tmp[i]['wallet_address']    
+    funds_share_i = beneficiaries_tmp[i]['funds_share']    
+    
+    # save the beneficiary dict in array (note that we don't store sensible data)
+    beneficiaries.append({'wallet_address': address_i, 'message_url': message_url_i, 'funds_share': funds_share_i})
 
+print "\nStep 3: Proof of Life"
 print "You will need to give signs of life periodically." 
 print "If you fail to so for a time greater than T, we'll assume you are dead."
-t_PoL = raw_input("Please enter a value for T (in days):")
+t_PoL = raw_input("Please enter a value for T (in days):\n")
 
 user_address = get_user_address()
 user_contract = LegacyUserContract(k, n, t_PoL, 0, beneficiaries, user_address)
@@ -97,7 +118,7 @@ print "After that, your beneficiaries will need their *personal decryption passw
 print "Backup this information carefully, and make sure to close this application."
 print "\n"
 for i in range (0, n):
-    print "- address " + beneficiaries[i].wallet_address
+    print "- address " + beneficiaries[i]['wallet_address']
     print "    secret piece: " + secret_pieces[i]
     print "    personal decryption password: " + personal_keys[i]
     print "\n" 
